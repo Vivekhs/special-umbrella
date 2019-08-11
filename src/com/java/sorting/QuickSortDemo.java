c12b8fbc92daadcd96576a0850a3634e5fa79d1e
Vivek Sinha <viveksinha461@gmail.com>
2019-08-11 17:37:11 +0530

    sorting added

---
+++ src/com/java/sorting/QuickSortDemo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java.sorting;
+
+/**
+ * @author vivekhs
+ *
+ */
+public class QuickSortDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
